b4420fa687e7e4b83eae9039fb553e530530a7b8
Kherae <cwaldridge@live.com>
2020-12-16 15:07:49 -0800

    r

---
+++ scripts/enhancedstorage.lua
@@ -0,0 +1,29 @@
+local enhancedstorageoldinit=init
+local enhancedstorageoldupdate=update
+local enhancedstorageolddie=die
+
+require("/scripts/shared/util.lua")
+require("/scripts/shared/objects/keepcontent.lua")
+require("/scripts/shared/objects/features.lua")
+require("/scripts/shared/objects/sortcontainer.lua")
+
+function init()
+	if enhancedstorageoldinit then enhancedstorageoldinit() end
+	itemConfig = {}
+	initContainer()
+
+	-- force to load proper uiConfig
+	if config.getParameter("uiConfig") then
+		object.setConfigParameter("uiConfig", "/interface/chests/chest<slots>.config")
+	end
+end
+
+function update(dt)
+	if enhancedstorageoldupdate then enhancedstorageoldupdate(dt) end
+	placeItems()
+end
+
+function die()
+	if die then enhancedstorageolddie() end
+	smashContainer("container")
+end
